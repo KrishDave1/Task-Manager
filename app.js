20e50e6afa1b0df2fa63726cd0527bfdac6dbc0b
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-01-30 15:37:35 +0530

    Add routes,controllers and middleware templates

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json()); // This helps us get the data in req.body.
+
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Hello World!')
+    // We can also setup a beautiful HTML page on some url in backend server.
+});
+
+app.use('/api/v1/tasks', tasks);
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+
+// All types of request executed here.
+// app.get('/api/v1/tasks') - get all the tasks
+// app.post('/api/v1/tasks') - create a new task
+// app.get('/api/v1/tasks/:id') - get a single task
+// app.patch('/api/v1/tasks/:id') - update task.
+// app.delete('/api/v1/tasks/:id') - delete task.
---
+++ controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req, res) => {
+    res.send('All items recieved');
+}
+
+module.exports = {
+    getAllTasks,
+};
